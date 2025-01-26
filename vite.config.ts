e0267563f1d9f572b32d5561c60da1bc2721ee6d
Alisultanov Shamil <Shamil.Alisultanov@transitcard.ru>
2025-01-26 16:09:48 +0300

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,63 +1,68 @@
-import { resolve } from 'path';
-
-/* eslint-disable import/no-extraneous-dependencies */
 import legacy from '@vitejs/plugin-legacy';
 import vue from '@vitejs/plugin-vue';
 import autoprefixer from 'autoprefixer';
+import { resolve } from 'path';
 import { defineConfig, type UserConfig } from 'vite';
-/* eslint-enable import/no-extraneous-dependencies */
+import { analyzer } from 'vite-bundle-analyzer';
 
 export default defineConfig((): UserConfig => {
-  return {
-    plugins: [
-      vue(),
-      legacy({
-        targets: ['defaults', 'not IE 11']
-      })
-    ],
+    return {
+        plugins: [
+            vue(),
+            legacy({
+                targets: ['defaults', 'not IE 11']
+            }),
+            analyzer(),
+        ],
 
-    css: { postcss: { plugins: [autoprefixer()] } },
-
-    server: {
-      port: 9000,
-
-      proxy: {
-        '/api': {
-          target: 'https://ppr-v3.transitcard.ru/',
-          changeOrigin: true
-        }
-      }
-    },
-
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            utils: ['date-fns', 'lodash-es', 'pinia'],
-            components: []
-          }
-        }
-      }
-    },
-
-    resolve: {
-      alias: [
-        // @/xxxx => src/xxxx
-        {
-          find: /^@\/(.+)/,
-          replacement: `${resolve(__dirname, 'src')}/$1`
+        css: {
+            postcss: {
+                plugins: [
+                    autoprefixer()
+                ]
+            }
         },
-        // #/type => types
-        {
-          find: /^#\/types$/,
-          replacement: `${resolve(__dirname, 'types')}`
+
+        server: {
+            port: 9000,
+
+            proxy: {
+                '/api': {
+                    target: 'https://ya.ru/',
+                    changeOrigin: true
+                }
+            }
         },
-        // #/types/xxxx => types/xxxx
-        {
-          find: /^#\/types\/(.+)/,
-          replacement: `${resolve(__dirname, 'types')}/$1`
+
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        utils: ['date-fns', 'lodash-es', 'pinia'],
+                        components: []
+                    }
+                }
+            }
+        },
+
+        resolve: {
+            alias: [
+                // @/xxxx => src/xxxx
+                {
+                    find: /^@\/(.+)/,
+                    replacement: `${resolve(__dirname, 'src')}/$1`
+                },
+                // #/type => types
+                {
+                    find: /^#\/types$/,
+                    replacement: `${resolve(__dirname, 'types')}`
+                },
+                // #/types/xxxx => types/xxxx
+                {
+                    find: /^#\/types\/(.+)/,
+                    replacement: `${resolve(__dirname, 'types')}/$1`
+                }
+            ]
         }
-      ]
-    }
-  };
+    };
 });
